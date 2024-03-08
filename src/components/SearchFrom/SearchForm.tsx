c11c22f2a603aa1a97472a968f5fea124432b494
Vlad-prog-byte <vlad2mickevich@gmail.com>
2024-03-08 11:05:00 +0300

    add onChange input

--- src/components/SearchFrom/SearchForm.tsx
+++ src/components/SearchFrom/SearchForm.tsx
@@ -1,10 +1,14 @@
 import "./styles.css";
-
+import { useState } from "react";
+import React from "react";
 export function SearchForm() {
+  const [search, setSearch] = useState<string>('');
+
+  const onChange = (e: React.FormEvent<HTMLInputElement>) => setSearch(e.currentTarget.value);
   return (
     <div className="searchForm">
       <form>
-        <input type="text" value="Terry" />
+        <input type="text" value={search} onChange={onChange}/>
       </form>
     </div>
   );
